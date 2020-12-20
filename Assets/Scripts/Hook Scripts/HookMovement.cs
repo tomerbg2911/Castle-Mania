2a3c1556d2f688d69614dea6c4933671b222b649
Tomer <tomer2911@gmail.com>
2020-12-20 10:59:24 +0200

    hook script

---
+++ Assets/Scripts/Hook Scripts/HookMovement.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookMovement : MonoBehaviour
+{
+    enum HookState
+    {
+        idle, // hook is not in player control
+        rotating, // hook is in player control (rotating up and down)
+        firedTowardsTarget, // the hook is moving towards a target
+        firedGoingBack, // the hook is moving back to HookPositionBeforeShooting
+    }
+
+    private HookState hookState; // current hook state
+    public Vector3 HookPositionBeforeShooting; // the hook's position before being fired
+
+    // rotation related vars
+    public Transform rotationAnchor;
+    public float rotationSpeed = 10f;
+    public float minRotationZ = 0f;
+    public float maxRotationZ = 360f;
+
+    // shooting related vars
+    public float shootingSpeed;
+    public float maxShootingDistanceOnX; // how far will the hook go during shooting
+
+
+
+    void Start()
+    {
+        // init variables
+        Time.timeScale = 1; // TODO: figure out why deltaTime equals 0 without this line
+        HookPositionBeforeShooting = transform.position;
+        hookState = HookState.rotating;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        HookShooting();
+    }
+
+    void Rotate(Vector3 axis)
+    {
+        if(hookState == HookState.rotating)
+        {
+            transform.RotateAround(rotationAnchor.position, axis, rotationSpeed);
+        }
+    }
+
+    void HookShooting()
+    {
+        Vector3 tempPosition = transform.position;
+
+        if (hookState == HookState.firedTowardsTarget)
+        { 
+            tempPosition -= transform.up * shootingSpeed * Time.deltaTime; 
+            if (tempPosition.x > HookPositionBeforeShooting.x + maxShootingDistanceOnX)
+            {
+                print("Stop");
+                hookState = HookState.firedGoingBack;
+            }
+        }
+
+        else if (hookState == HookState.firedGoingBack)
+        { 
+            tempPosition += transform.up * shootingSpeed * Time.deltaTime;
+            if ((tempPosition.x - HookPositionBeforeShooting.x) < 0.1f)
+            {
+                tempPosition = HookPositionBeforeShooting;
+                hookState = HookState.rotating;
+            }
+        }
+
+        transform.position = tempPosition;
+    }
+
+    void GetInput()
+    {
+        if (Input.GetKey(KeyCode.DownArrow) && transform.rotation.z > minRotationZ)
+        {
+            Rotate(Vector3.back);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && transform.rotation.z < maxRotationZ)
+        {
+            Rotate(Vector3.forward);
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && hookState == HookState.rotating)
+        {
+            HookPositionBeforeShooting = transform.position;
+            print(HookPositionBeforeShooting);
+            hookState = HookState.firedTowardsTarget;
+        }
+    }
+}
---
+++ Assets/Scripts/Hook Scripts/RopeRenderer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeRenderer : MonoBehaviour
+{
+
+    private LineRenderer lineRenderer;
+    public Transform startPosition;
+    private float lineWidth = 0.05f;
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = lineWidth;
+        lineRenderer.enabled = false;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void renderLine(Vector3 endPosition, bool enableRenderer)
+    {
+        if(enableRenderer)
+        {
+            if(!lineRenderer.enabled)
+            {
+                lineRenderer.enabled = true;
+            }
+
+            lineRenderer.positionCount = 2;
+        }
+
+        else
+        {
+            lineRenderer.positionCount = 0;
+            if (lineRenderer.enabled)
+            {
+                lineRenderer.enabled = false;
+            }
+
+        }
+
+        if(lineRenderer.enabled)
+        {
+            Vector3 temp = startPosition.position;
+            temp.z = 0f;
+
+            startPosition.position = temp;
+
+            temp = endPosition;
+            temp.z = 0f;
+
+            endPosition = temp;
+
+            lineRenderer.SetPosition(0, startPosition.position);
+            lineRenderer.SetPosition(1, endPosition);
+        }
+    }
+}
