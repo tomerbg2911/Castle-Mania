62db1ff7e7613c8d56d0aeadd3c88bb1d24ca766
Ronbs13 <xron12123@gmail.com>
2020-12-21 03:13:30 +0200

    some new scripts

---
+++ Assets/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public float collectableTimeInterval;
+    public float collectableMinX;
+    public float collectableMaxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Tower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public int healthPoints = 100;
+    public bool isGateOpen = false;
+
+    // soldiers related vars
+    public int maxNumOfSoldiers = 5;
+    public int currentNumOfSoldiers = 5;
+
+    // keyboard keys
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode shoot;
+    public KeyCode switchWeapon;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
