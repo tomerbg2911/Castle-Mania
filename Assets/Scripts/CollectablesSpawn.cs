60f1b367ba0a3b7adb41dd581d2e5bab021df7a4
Tomer <tomer2911@gmail.com>
2020-12-22 00:45:44 +0200

    collectable spawn script

---
+++ Assets/Scripts/CollectablesSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectablesSpawn : MonoBehaviour
+{
+    // collectables spawn related vars
+    public GameObject CollectableGameObject;
+    public float spawnTimeInterval;
+    public float minXPosition;
+    public float maxXPosition;
+    public float minFallingSpeed;
+    public float maxFallingSpeed;
+    public float initYPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 2f, spawnTimeInterval);
+    }
+
+
+    void Spawn()
+    {
+        float xPosition = Random.Range(minXPosition, maxXPosition);
+        Vector3 position = new Vector3(xPosition, initYPosition);
+        float fallingSpeed = Random.Range(minFallingSpeed, maxFallingSpeed);
+
+        GameObject newCollectable = Instantiate(CollectableGameObject, position, Quaternion.identity) as GameObject;
+        ManaCollectable manaCollectable = newCollectable.GetComponent<ManaCollectable>();
+        manaCollectable.fallingSpeed = fallingSpeed;
+    }
+}
