1cb757b076ae6ae8db426e1691ec4072faaa0740
Tomer <tomer2911@gmail.com>
2020-12-26 00:04:51 +0200

    Revert "Merge branch 'Aviv-Dev'"
    This reverts commit c6e0bf16c42ed496f782674b4f833835518e4ab6, reversing
    changes made to 9abeefd60ccf65a03582076e1f3bf1fbeaf0589e.

---
+++ Assets/Scripts/Weapons/Dragon Scripts/DragonFire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonFire : MonoBehaviour
+{
+    public Vector3 fireTarget;
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTowardsTarget();
+
+    }
+
+    void MoveTowardsTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, fireTarget, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "gate")
+        {
+            Tower towerGotHit = collision.transform.GetComponentInParent<Tower>();
+            towerGotHit.onDragonFireHitGate();
+            Destroy(gameObject);
+        }
+    }
+}
