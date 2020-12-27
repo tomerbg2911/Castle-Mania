874d767588a18ba63e94811b3e9abb08a7569958
Tomer <tomer2911@gmail.com>
2020-12-27 21:43:46 +0200

    added delay before shooting to bazooka

--- Assets/Scripts/Weapons/Bazzoka Scripts/Shooting.cs
+++ Assets/Scripts/Weapons/Bazzoka Scripts/Shooting.cs
@@ -17,7 +17,9 @@ public class Shooting : MonoBehaviour
     public float MinLaunchForce = 15f;
     public float MaxLaunchForce = 30f;
     public float MaxChargeTime = 0.75f;
-
+    public float delayBetweenShooting = 1f;
+    
+    private bool canShoot;
     private KeyCode FireButton;
     private float currentLaunchForce;
     private float ChargeSpeed;  
@@ -27,6 +29,7 @@ public class Shooting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        canShoot = true;
         FireButton = GetComponentInParent<Tower>().shoot;
         ChargeSpeed = (MaxLaunchForce - MinLaunchForce) / MaxChargeTime;
         AimSlider.gameObject.SetActive(false);
@@ -37,32 +40,40 @@ public class Shooting : MonoBehaviour
     void Update()
     {
         AimSlider.value = MinLaunchForce;
-        if (currentLaunchForce >= MaxLaunchForce && !Fired)
+        if(canShoot)
         {
-            currentLaunchForce = MaxLaunchForce;
-            Fire();
-        }
-        else if (Input.GetKeyDown(FireButton))
-        {
-            Fired = false;
-            currentLaunchForce = MinLaunchForce;
-            AimSlider.gameObject.SetActive(true);
-            // ShootingAudio.clip = ChargingClip;
-            // ShootingAudio.Play();
-        }
-        else if (Input.GetKey(FireButton) && !Fired)
-        {
-            currentLaunchForce += ChargeSpeed * Time.deltaTime;
-            //Debug.Log("Current launch force: " + currentLaunchForce);
-            AimSlider.value = currentLaunchForce;
-           
-        }
-        else if (Input.GetKeyUp(FireButton) && !Fired)
-        {
-            Fire();
-          //  Debug.Log("Current launch force: " + currentLaunchForce);
-        }
+            if(currentLaunchForce >= MaxLaunchForce && !Fired)
+            {
+                currentLaunchForce = MaxLaunchForce;
+                Fire();
+            }
+            else if(Input.GetKeyDown(FireButton))
+            {
+                Fired = false;
+                currentLaunchForce = MinLaunchForce;
+                AimSlider.gameObject.SetActive(true);
+                // ShootingAudio.clip = ChargingClip;
+                // ShootingAudio.Play();
+            }
+            else if(Input.GetKey(FireButton) && !Fired)
+            {
+                currentLaunchForce += ChargeSpeed * Time.deltaTime;
+                //Debug.Log("Current launch force: " + currentLaunchForce);
+                AimSlider.value = currentLaunchForce;
 
+            }
+            else if(Input.GetKeyUp(FireButton) && !Fired)
+            {
+                Fire();
+                //  Debug.Log("Current launch force: " + currentLaunchForce);
+            }
+        }
+    }
+
+    IEnumerator ShootDelay()
+    {
+        yield return new WaitForSeconds(delayBetweenShooting);
+        canShoot = true;
     }
 
     private void Fire()
@@ -80,5 +91,8 @@ public class Shooting : MonoBehaviour
         // play flare animation
         FlareTransform.GetComponent<Animator>().SetTrigger("Shoot");
 
+        // start shooting delay
+        canShoot = false;
+        StartCoroutine(ShootDelay());
     }
 }
