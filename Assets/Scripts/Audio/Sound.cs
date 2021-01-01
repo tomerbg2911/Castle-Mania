ccca7b8addc19270071acb2406a6031042b36d24
Tomer <tomer2911@gmail.com>
2021-01-01 12:05:45 +0200

    Sounds and Audio Manager

---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable] 
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    public bool loop;
+
+    [Range(0f, 1f)]
+    public float volume = 1f;
+    [Range(.1f, 3f)]
+    public float pitch = 1f;
+
+    [HideInInspector]
+    public AudioSource source;
+}
