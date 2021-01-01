de62ea8ff55f02879e8b9c4e7fa9f4358bd50cde
sikth394 <avivw08@gmail.com>
2021-01-02 01:42:46 +0200

    general fixes

--- Assets/Scripts/Weapons/Bazzoka Scripts/Bullet.cs
+++ Assets/Scripts/Weapons/Bazzoka Scripts/Bullet.cs
@@ -10,7 +10,7 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 6f);
     }
 
     // Update is called once per frame
